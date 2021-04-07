af077ba7e6316c2f25430934818112f663299669
nilaykd <nilaykdoshi@gmail.com>
2021-04-07 13:09:16 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.markdown("# Market Basket Analysis")
 # """
 # )
 
-uploaded_file = st.file_uploader("Choose a file", type=['xlsx'])
+uploaded_file = st.file_uploader("Choose a file", type=['xls'])
 if uploaded_file is not None:
 
     # if default_csv == 'INTEGRATED-DATASET.csv':
